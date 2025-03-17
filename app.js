7df942e7c83894575f107f8be745ab40e0ac8130
CarlosLlanoT <carlosllanotrecans@gmail.com>
2025-03-17 01:50:10 -0300

    agregar funcion agregarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [] //array amigos
+
+function agregarAmigo(){
+    const inputAmigo = document.getElementById("amigo");
+    const nombreAmigo = inputAmigo.ariaValueMax.trim();
+    if(nombreAmigo == ""){ //verificar
+        alert("Por favor inserte un nombre.");
+        return;
+    }
+    if(amigos.includes(nombreAmigo)) {
+        alert('El nombre ${nombreAmigo} ya esta en la lista');
+        return
+    }
+    amigos.push(nombreAmigo)
+    inputAmigo.value= ""; // verificar
+    
+    actualizarlista();
+}
